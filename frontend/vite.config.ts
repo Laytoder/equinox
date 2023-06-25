2048f33e837e807c8593d9ec658f178cd809a7d8
Lakshya Gupta <laytoder@gmail.com>
2023-06-26 03:53:16 +0530

    testing gh pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,8 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/equinox",
   plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
-    },
-    host: true,
-    strictPort: true,
-    port: 8000,
-  },
 });
